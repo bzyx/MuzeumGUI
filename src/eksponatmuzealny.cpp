d701c921ff942d4e8fae2c94e7df393445b1d23e
Marcin Jabrzyk <bzyx90@gmail.com>
2011-05-23 17:57:44 +0200

    Very first EksponatMuzealny implementation

--- src/eksponatmuzealny.cpp
+++ src/eksponatmuzealny.cpp
@@ -4,4 +4,24 @@
 
 EksponatMuzealny::EksponatMuzealny()
 {
+    m_id = EksponatMuzealny::m_lastid++;
+    m_typ = EksponatMuzealny::NieOkreslono;
+    m_nazwa = "";
+    m_opis = "";
+    m_rok = 0;
+}
+
+int id()
+{
+    return m_id;
+}
+
+int nazwa()
+{
+    return m_nazwa;
+}
+
+EksponatMuzealny::Typ typ()
+{
+    return m_typ;
 }
--- src/eksponatmuzealny.h
+++ src/eksponatmuzealny.h
@@ -1,12 +1,32 @@
-
+#include <string>;
 
 #ifndef EKSPONATMUZEALNY_H
 #define EKSPONATMUZEALNY_H
 
+
+
 class EksponatMuzealny
 {
 public:
+    typedef enum {NieOkreslono=-1, Obraz=0, PrzedmiotUzytkowy, Rzezba, Mebel, Rekopis, Starodruk } Typ;
     EksponatMuzealny();
+    int id();
+    int nazwa();
+
+protected:
+    static int m_lastid;
+    int m_id;
+    Typ m_typ;
+    std::string m_nazwa;
+    //enum stan {EksonatMuzealny::Wystawiony, EksopantMuzealny::WMagazynie}
+    std::string m_opis;
+    int m_rok;
+
+private:
+
+
 };
+
+int m_lastid = 0;
 
 #endif // EKSPONATMUZEALNY_H
