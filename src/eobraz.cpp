729dca356b1aeb037bff4dbf6d0548ff7966af8b
Marcin Jabrzyk <bzyx90@gmail.com>
2011-06-11 00:07:27 +0200

    Poprawione formatowanie pliku.

--- src/eobraz.cpp
+++ src/eobraz.cpp
@@ -3,75 +3,30 @@
 #include <QString>
 #include "eobraz.h"
 
-/**
- * @brief
- *
- * @return float
- */
 float EObraz::getWysokosc(){
     return m_wysokosc;
 }
 
-/**
- * @brief
- *
- * @param w
- */
 void EObraz::wysokosc(float w){
     m_wysokosc = w;
 }
 
-/**
- * @brief
- *
- * @return float
- */
 float EObraz::getSzerokosc(){
     return m_szerokosc;
 }
 
-/**
- * @brief
- *
- * @param s
- */
 void EObraz::szerokosc(float s){
     m_szerokosc = s;
 }
 
-/**
- * @brief
- *
- * @return std::string
- */
 std::string EObraz::getAutor(){
     return m_autor;
 }
 
-/**
- * @brief
- *
- * @param autor
- */
 void EObraz::autor(std::string autor){
     m_autor = autor;
 }
 
-/**
- * @brief
- *
- * @param w
- * @param s
- * @param autor
- * @param nazwa
- * @param wystawiony
- * @param opis
- * @param polozenie
- * @param wartosc
- * @param typ
- * @param formatDaty
- * @param data
- */
 EObraz::EObraz(float w, float s, std::string autor,
                std::string nazwa, bool wystawiony, std::string opis,
                std::string polozenie, int wartosc,
@@ -83,30 +38,12 @@ EObraz::EObraz(float w, float s, std::string autor,
     m_autor = autor;
 }
 
-/**
- * @brief
- *
- */
-/**
- * @brief
- *
- */
 EObraz::EObraz():EksponatMuzealny(){
     m_wysokosc = 0.0;
     m_szerokosc = 0.0;
     m_autor = "";
 }
 
-/**
- * @brief
- *
- * @return QStringList
- */
-/**
- * @brief
- *
- * @return QStringList
- */
 QStringList EObraz::getAtrybuty(){
     QStringList nowa = getPodstawoweAtrybuty();
     QStringList tmp;
@@ -116,21 +53,7 @@ QStringList EObraz::getAtrybuty(){
     nowa.append(tmp);
     return nowa;
 }
-/**
- * @brief
- *
- * @return QStringList
- */
-/**
- * @brief
- *
- * @return QStringList
- */
-/**
- * @brief
- *
- * @return QStringList
- */
+
 QStringList EObraz::getHeaders(){
     QStringList nowa = getPodstawoweHeaders();
     nowa << "Wysokoœæ"
@@ -139,19 +62,7 @@ QStringList EObraz::getHeaders(){
     return nowa;
 }
 
-/**
- * @brief
- *
- * @return EksponatMuzealny::r2f
- */
-/**
- * @brief
- *
- * @return EksponatMuzealny::r2f
- */
 EksponatMuzealny::r2f EObraz::saveElement(){
-    //    typedef QPair<QString, QString> krotka;
-    //    typedef QList<krotka> r2f;
     r2f data;
     QStringList h = getHeaders();
     QStringList a = getAtrybuty();
@@ -160,14 +71,6 @@ EksponatMuzealny::r2f EObraz::saveElement(){
     return data;
 }
 
-/**
- * @brief
- *
- */
-/**
- * @brief
- *
- */
 EObraz::~EObraz(){
 
 }
