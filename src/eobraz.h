3233b99c5aeed23730c7d736e62e15e28fd3b289
Marcin Jabrzyk <bzyx90@gmail.com>
2011-06-10 15:23:19 +0200

    Class EObraz in new form

--- src/eobraz.h
+++ src/eobraz.h
@@ -4,19 +4,96 @@
 #ifndef EOBRAZ_H
 #define EOBRAZ_H
 
+/**
+ * @brief
+ *
+ */
 class EObraz : public EksponatMuzealny
 {
-public:
-    EObraz(std::string nazwa, std::string opis, int rok);
-    int szerokosc();
-    int wysokosc();
-    void setSzerokoscWysokosc(int, int);
-    int id();
-protected:
-    int m_szerokosc;
-    int m_wysokosc;
 private:
+    float m_wysokosc; /**< TODO */
+    float m_szerokosc; /**< TODO */
+    std::string m_autor; /**< TODO */
+public:
+    /**
+     * @brief
+     * Zwraca wyskokoœæ
+     */
+    float getWysokosc();
+    /**
+     * @brief
+     * Ustawia wysokoœæ
+     * @param w
+     */
+    void wysokosc(float w);
+    /**
+     * @brief
+     * Zwraca szerokoœæ
+     */
+    float getSzerokosc();
+    /**
+     * @brief
+     * Ustawia szerokoœæ
+     * @param s
+     */
+    void szerokosc(float s);
+    /**
+     * @brief
+     * Zwraca autora
+     */
+    std::string getAutor();
+    /**
+     * @brief
+     * Ustawia autora
+     * @param autor
+     */
+    void autor(std::string autor);
+/**
+ * @brief
+ * Konstruktor pe³ny =
+ * w,s,autor + EksponatMuzealny
+ * @param w
+ * @param s
+ * @param autor
+ * @param nazwa
+ * @param wystawiony
+ * @param opis
+ * @param polozenie
+ * @param wartosc
+ * @param typ
+ * @param formatDaty
+ * @param data
+ */
+    EObraz(float w, float s, std::string autor,
+           std::string nazwa, bool wystawiony, std::string opis,
+           std::string polozenie, int wartosc,
+           Typ typ, FormatDaty formatDaty, std::string data);
+/**
+ * @brief
+ * Konstruktor romyœlny
+ */
     EObraz();
+    /**
+     * @brief
+     * Zwraca listê atrybutów z dodatkowymi
+     * atrybutami
+     */
+    QStringList getAtrybuty();
+    /**
+     * @brief
+     * Zwraca heder z dodatkowymi kolumnami
+     */
+    QStringList getHeaders();
+    /**
+     * @brief
+     * Przygotowuje rekord do zapisu
+     */
+    r2f saveElement();
+    /**
+     * @brief
+     * Destruktor
+     */
+    ~EObraz();
 };
 
 #endif // EOBRAZ_H
