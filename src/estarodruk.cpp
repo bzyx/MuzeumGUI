0b0d1991123729414d0f2506990b04bc44ba2d71
Marcin Jabrzyk <bzyx90@gmail.com>
2011-06-12 16:41:54 +0200

    Class EStarodruk InitVerision.

--- src/estarodruk.cpp
+++ src/estarodruk.cpp
@@ -2,6 +2,18 @@
 
 #include "estarodruk.h"
 
-EStarodruk::EStarodruk()
-{
-}
+std::string EStarodruk::getAutor();
+void EStarodruk::autor(std::string autor);
+std::string EStarodruk::getStreszczenie();
+void EStarodruk::sterszczenie(std::string streszczenie);
+int EStarodruk::getLiczbaStron();
+void EStarodruk::liczbaStron(int liczbaStron);
+EStarodruk::EStarodruk(std::string autor, std::string streszczenie, int liczbaStron,
+       std::string nazwa, bool wystawiony, std::string opis,
+       std::string polozenie, int wartosc,
+       Typ typ, FormatDaty formatDaty, std::string data);
+EStarodruk::EStarodruk();
+QStringList EStarodruk::getAtrybuty();
+QStringList EStarodruk::getHeaders();
+EksponatMuzealny::r2f EStarodruk::saveElement();
+EStarodruk::~EStarodruk();
--- src/estarodruk.h
+++ src/estarodruk.h
@@ -3,10 +3,94 @@
 #ifndef ESTARODRUK_H
 #define ESTARODRUK_H
 
+/**
+ * @brief
+ *
+ */
 class EStarodruk : public EksponatMuzealny
 {
+private:
+    std::string m_autor; /**< TODO */
+    std::string m_streszczenie; /**< TODO */
+    int m_liczbaStron; /**< TODO */
 public:
+    /**
+     * @brief
+     *
+     */
+    std::string getAutor();
+    /**
+     * @brief
+     *
+     * @param autor
+     */
+    void autor(std::string autor);
+    /**
+     * @brief
+     *
+     */
+    std::string getStreszczenie();
+    /**
+     * @brief
+     *
+     * @param streszczenie
+     */
+    void sterszczenie(std::string streszczenie);
+    /**
+     * @brief
+     *
+     */
+    int getLiczbaStron();
+    /**
+     * @brief
+     *
+     * @param liczbaStron
+     */
+    void liczbaStron(int liczbaStron);
+/**
+ * @brief
+ *
+ * @param autor
+ * @param streszczenie
+ * @param liczbaStron
+ * @param nazwa
+ * @param wystawiony
+ * @param opis
+ * @param polozenie
+ * @param wartosc
+ * @param typ
+ * @param formatDaty
+ * @param data
+ */
+    EStarodruk(std::string autor, std::string streszczenie, int liczbaStron,
+           std::string nazwa, bool wystawiony, std::string opis,
+           std::string polozenie, int wartosc,
+           Typ typ, FormatDaty formatDaty, std::string data);
+/**
+ * @brief
+ *
+ */
     EStarodruk();
+    /**
+     * @brief
+     *
+     */
+    QStringList getAtrybuty();
+    /**
+     * @brief
+     *
+     */
+    QStringList getHeaders();
+    /**
+     * @brief
+     *
+     */
+    r2f saveElement();
+    /**
+     * @brief
+     *
+     */
+    ~EStarodruk();
 };
 
 #endif // ESTARODRUK_H
