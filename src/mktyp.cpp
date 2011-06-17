19db423249d016e5c7ee0da10c6d9728b253418e
Marcin Jabrzyk <bzyx90@gmail.com>
2011-06-17 21:16:41 +0200

    Removing the bug with memory.

--- src/mktyp.cpp
+++ src/mktyp.cpp
@@ -10,24 +10,23 @@ void MKTyp::setTyp(Meta::Typ typ){
     m_typ=typ;
 }
 EksponatMuzealny* MKTyp::operator[](int id){
-   return getObjectFor(m_typ,id);
+    return getObjectFor(m_typ,id);
 }
 
 void MKTyp::updateMKTyp(){
     for (int i=0; i< MK::getInstance().count();++i){
-        qDebug() << (int) ((MK::getInstance()[i])->getTyp());
+        //qDebug() << (int) ((MK::getInstance()[i])->getTyp());
         (m_Types[ ( (MK::getInstance()[i])->getTyp() ) ]).push_back(i);
-        //(m_Types[((MK::getInstance()[i])->getTyp())]).push_back(i);
     }
-
-    //(m_Types[Meta::Obraz]).push_back(1);
-    //(m_Types[Meta::Obraz]).push_back(2);
 }
 int MKTyp::count(Meta::Typ typ){
     return m_Types[typ].size();
 }
 EksponatMuzealny* MKTyp::getObjectFor(Meta::Typ typ,int id){
-    return MK::getInstance()[((m_Types[typ])[id])];
+    if (m_Types[typ].size() >0){
+        return MK::getInstance()[((m_Types[typ])[id])];
+    } else
+        return NULL;
 }
 MKTyp::MKTyp()
 {
