89d6d6920b8b1a442f8e540493f6acaa0064ccb1
Marcin Jabrzyk <bzyx90@gmail.com>
2011-06-21 22:46:51 +0200

    First try to get FormatDaty of EksponatMuzealny

--- src/meta.h
+++ src/meta.h
@@ -6,6 +6,8 @@ class Meta
 public:
     typedef enum {NieOkreslonoTypu=-1, Obraz=5, PrzedmiotUzytkowy,
                   Rzezba, Mebel, Rekopis, Starodruk } Typ;
+
+    typedef enum {w=5,r,mr,dmr} FormatDaty;
     Meta();
 };
 
