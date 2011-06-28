ead058015e8272ec2c9799aa2ae92c275b5aa504
Marcin Jabrzyk <bzyx90@gmail.com>
2011-06-28 19:05:44 +0200

    First page?

--- main.cpp
+++ main.cpp
@@ -14,6 +14,31 @@
 **
 ****************************************************************************/
 
+/*! \mainpage Muzeum - Baza danych
+ *
+ * \section intro_sec Za³o¿enia programu
+ *
+ * ZAD. 8) Napisaæ program - obiektow¹ bazê danych o eksponatach zgromadzonych
+ * w muzeum. Za³o¿yæ, ¿e zbiory muzealne zawieraj¹ takie eksponaty jak: obrazy,
+ * rzeŸby, meble, przedmioty u¿ytkowe, zabytkowe rêkopisy i starodruki. Nale¿y
+ * umo¿liwiæ wprowadzanie danych, zapisywanie i odczytywanie z dysku.
+
+ * WSKAZÓWKI i UWAGI:
+ * - Ka¿da grupa eksponatów powinna byæ reprezentowana przez odpowiedni¹ klasê
+ * zapewniaj¹c¹ takie us³ugi jak edycja danych zwi¹zanych z obiektem danej klasy,
+ * zapis i odczytywanie.
+ * - Wszystkie klasy powinny zostaæ wyprowadzone ze wspólnej klasy bazowej
+ * Eksponat Muzealny.
+ * - W pamiêci utworzyæ strukturê dynamiczn¹ - listê zawieraj¹ca elementy typu
+ * Eksponat Muzealny.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
